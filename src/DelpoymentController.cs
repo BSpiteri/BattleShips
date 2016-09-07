591d0178e2cc46753114489f58286eaf732ad2f2
BSpiteri <100590161@student.swin.edu.au>
2016-09-07 23:07:32 +1000

    more fixed code

--- src/DelpoymentController.cs
+++ src/DelpoymentController.cs
@@ -97,7 +97,7 @@ static class DeploymentController
 		int row = 0;
 		int col = 0;
 			row = Convert.ToInt32(Math.Floor((mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
-		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions. FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
+		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
 
 			if (row >= 0 & row < GameController.HumanPlayer.PlayerGrid.Height) {
 				if (col >= 0 & col < GameController.HumanPlayer.PlayerGrid.Width) {
@@ -134,7 +134,7 @@ static class DeploymentController
 			//DrawShips
 			foreach (ShipName sn in Enum.GetValues(typeof(ShipName))) {
 				int i = 0;
-				i = (int)sn - 1;
+				i = ((int)sn) - 1;
 				if (i >= 0) {
 					if (sn == _selectedShip) {
 						SwinGame.DrawBitmap(GameResources.GameImage("SelectedShip"), SHIPS_LEFT, SHIPS_TOP + i * SHIPS_HEIGHT);
@@ -168,7 +168,7 @@ static class DeploymentController
 	{
 		foreach (ShipName sn in Enum.GetValues(typeof(ShipName))) {
 			int i = 0;
-				i = (int)sn - 1;
+				i = ((int)sn) - 1;
 
 				if (UtilityFunctions.IsMouseInRectangle(SHIPS_LEFT, SHIPS_TOP + i * SHIPS_HEIGHT, SHIPS_WIDTH, SHIPS_HEIGHT)) {
 				return sn;
